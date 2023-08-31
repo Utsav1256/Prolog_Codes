b66d832ec14c87ebff04b4ae5db8f742bd845d4b
utsav kumar <utsavkumar5462@gmail.com>
2023-08-31 15:15:20 +0530

    parent_of relation

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Prolog_Codes
+
+tom ->
+- bob
+pam ->
+- bob
+- liz
+
+bob ->
+- ann
+- pat
+
+pat ->
+- jim
---
+++ relation.pl
@@ -0,0 +1,15 @@
+male(bob).
+male(pat).
+male(jim).
+female(pam).
+female(liz).
+female(ann).
+
+# parent of relation
+parent_of(pam, bob).
+parent_of(tom, bob).
+parent_of(tom, liz).
+parent_of(bob, ann).
+parent_of(bob, pat).
+parent_of(pat, jim).
+
